54748f2d5ebb048d8040933fb633ff0ccfb38bbc
Scr1pti3 <52656050+scr1pti3@users.noreply.github.com>
2022-10-06 09:48:55 +0700

    Added CallStub

--- Source/Handler.h
+++ Source/Handler.h
@@ -15,28 +15,42 @@ namespace Cent::Concept
                          std::same_as<T, Type::ExprShrd>  ||
                          std::same_as<T, Type::ParseResult> ||
                          std::same_as<T, Type::ParseResult&&>;
+    
+    template<typename Fn, typename T>
+    concept Handler = std::invocable<Fn, T>;
 }
 
 namespace Cent
 {
     using namespace Concept;
 
-    template<Handleable T, class Fn1, class Fn2> requires std::invocable<Fn1, T> && std::invocable<Fn2, T>
+    template<Handleable T, Handler<T> Fn> inline 
+    void _CallStub(Fn p_fn, T&& p_data) requires std::invocable<Fn, T>
+    {
+        using namespace Type;
+
+        if constexpr (std::same_as<T, ParseResult>)
+            p_fn(p_data);
+        else 
+            p_fn(p_data);
+    }
+
+    template<Handleable T, Handler<T> Fn1, Handler<T> Fn2> 
         T handle(T &&p_data, Fn1 p_pass, Fn2 p_fail)
     {
         if (Tool::is_valid(p_data))
-            p_pass(p_data);
+            _CallStub(p_pass, std::move(p_data));
         else
-            p_fail(p_data);
+            _CallStub(p_fail, std::move(p_data));
 
         return p_data; 
     }
 
-    template<Handleable T, class Fn1> requires std::invocable<Fn1, T> 
+    template<Handleable T, Handler<T> Fn1>
         T handle(T &&p_data, Fn1 p_pass)
     {
         if (Tool::is_valid(p_data))
-            p_pass(p_data);
+            _CallStub(p_pass, std::move(p_data));
         
         return p_data; 
     }
