912eff38369fc7da0184d6f5bbc0b54ce9f456a9
Scr1pti3 <52656050+scr1pti3@users.noreply.github.com>
2022-10-08 08:37:34 +0700

    Added stmt

--- Source/Cent.cpp
+++ Source/Cent.cpp
@@ -1,15 +1,15 @@
 #include "Cent.hpp"
 
 #include <iostream>
-#include <concepts>
-#include <type_traits>
-
 
 using namespace Cent;
 
 
 int main(int argc, char *argv[]) 
 {
+
+
+
     if (argc == 2)
     {
         auto content = Tool::file2string(argv[1]);
